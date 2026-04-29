547901ed51d6a6231b82bf4d6d015e22887edd22
izbuno <justtib.channel@gmail.com>
2026-04-28 22:17:51 -0300

    torre_26-04.py

--- torre_26-04.py
+++ torre_26-04.py
@@ -2,17 +2,14 @@ def resolver_hanoi(n, origem, destino, auxiliar):
     if n == 1:
         print(f"Disco 1: {origem} ➔ {destino}")
         return
-    resolver_hanoi(n - 1, origem, auxiliar, destino)
-    
 
+    resolver_hanoi(n - 1, origem, auxiliar, destino)
     print(f"Disco {n}: {origem} ➔ {destino}")
-    
     resolver_hanoi(n - 1, auxiliar, destino, origem)
 
-# Execução
-n_discos = 3
-print(f"Resolvendo para {n_discos} discos:")
-resolver_hanoi(n_discos, 'Haste A', 'Haste C', 'Haste B')
+discos = int(input("Número de discos: "))
 
-total_movimentos = 2**n_discos - 1
-print(f"\nTotal de movimentos: {total_movimentos}")
+print(f"\nResolvendo para {discos} discos:")
+resolver_hanoi(discos, 'Haste A', 'Haste C', 'Haste B')
+
+print(f"\nTotal de movimentos: {2**discos - 1}")
